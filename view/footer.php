4cb206d3e11bba2cacebf6d71b68d795261f0642
Denis Gois <denisdwtg@gmail.com>
2018-02-07 16:43:18 -0500

    Remove integraty and crossoring fields

--- view/footer.php
+++ view/footer.php
@@ -6,7 +6,7 @@
     </p>
 </footer>
 
-<script src="js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="js/jquery-3.2.1.slim.min.js"></script>
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.js"></script>
 </body>
