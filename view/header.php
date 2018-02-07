85281b01e93d5ab687444252328471b9abed74dd
Denis Gois <denisdwtg@gmail.com>
2018-02-07 18:29:25 -0500

    Remove bootstrap

--- view/header.php
+++ view/header.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
 
