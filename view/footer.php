b27135e34cb2ec5fd78a3b2015e03f6249c75504
Denis Gois <denisdwtg@gmail.com>
2018-02-07 16:42:17 -0500

    Add bootstrap javascript files

--- view/footer.php
+++ view/footer.php
@@ -5,5 +5,9 @@
         &copy; Copyright 2018 by Orion Group
     </p>
 </footer>
+
+<script src="js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.js"></script>
 </body>
 </html>
