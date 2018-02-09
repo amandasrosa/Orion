69953ac249934c3b8c7de7a0d49ebdc615a97b01
Amanda S Rosa <amandasrosa@hotmail.com>
2018-02-09 17:56:35 -0500

    inicio do editar subject e questions

--- editQnS.php
+++ editQnS.php
@@ -1,13 +1,46 @@
 <?php 
 
-
+require_once('model/database.php');
+require_once('model/question_db.php');
+require_once('model/subject_db.php');
 
 include 'view/header.php'; ?>
 
 <section>
 
+<?php if (isset($_POST['editSubjects'])) { 
+
+	$subjects = get_subjects();
+
+	foreach ($subjects as $subject) { ?>
+
+	<input type="text" class="input-user-resgistration" name="<?php echo htmlspecialchars($subject['subject_id']);?>" value="<?php echo htmlspecialchars($subject['description']); ?>"
+		
+	<?php }?>
 
 
+<?php } ?>
+
+
+
+
+<?php if (isset($_POST['editQuestions'])) { 
+
+	$subjectId = $_POST['subjectRadio'];
+	$questions = get_questions_by_subject($subjectId);
+
+	//print_r($questions);
+	foreach ($questions as $question) { ?> 
+
+
+	<input type="text" class="input-user-resgistration" name="<?php echo htmlspecialchars($question['question_id']);?>" value="<?php echo htmlspecialchars($question['description']); ?>"
+		
+	<?php }?>
+
+
+
+
+<?php } ?>
 
 </section>
 
