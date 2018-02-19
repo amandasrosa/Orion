42dc9a7d6a75dc6c222810e6a455e7ef0a210158
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-02-19 18:56:43 -0500

    Implement click on add subject button

--- editQnS.php
+++ editQnS.php
@@ -50,9 +50,55 @@ if (!isset($subjectId)) { $subjectId = "1"; }
             event.preventDefault();
             createFormWithParametersAndSubmit('deleteQuestion.php', 'post', 'questionIdToDelete', questionId);
         };
-        function deleteSubject(event, questionId){
+    
+        function deleteSubject(event, subjectId){
             event.preventDefault();
         };
+    
+        function addSubject(event) {
+            event.preventDefault();
+            
+			var table = document.getElementById("tableSubjects");
+			var id = table.rows.length;
+			var row1 = table.insertRow(id);
+            
+			var idCell = row1.insertCell(0);
+			var subject = row1.insertCell(1);
+			var buttonCell = row1.insertCell(2);
+
+            idCell.innerHTML = "new";
+            
+			subject.classList.add("table-cell-question");
+            var textInput = document.createElement("INPUT");
+			textInput.setAttribute("type", "text");
+			textInput.name = id;
+            textInput.classList.add("input-form", "input-100");
+			textInput.required;
+            textInput.focus();
+			subject.appendChild(textInput);
+            
+            var button = document.createElement("INPUT");
+            button.setAttribute("type", "image");
+            button.src = "images/ok-icon.png";
+            button.alt = "Button";
+            button.height = "40";
+            button.width = "40";
+            button.classList.add("alignBottomImg", "cursor");
+            button.value = "e" + id;
+            button.addEventListener("click", function(ev) {
+				ev.preventDefault();
+                saveSubject(this);
+			});
+			buttonCell.appendChild(button);
+        }
+    
+        function saveSubject(button) {
+            button.src = "images/delete-icon.png";
+            button.addEventListener("click", function(ev) {
+                ev.preventDefault();
+                deleteSubject(event);
+            });
+        }
     </script>
 <section>
 
@@ -62,7 +108,7 @@ if (!isset($subjectId)) { $subjectId = "1"; }
 	$subjects = get_subjects();
 ?>
     
-    <table class="table-edit" id="tableQuestions" align="center">
+    <table class="table-edit" id="tableSubjects">
         <thead>
             <tr>
                 <th>Id</th>
@@ -75,13 +121,13 @@ if (!isset($subjectId)) { $subjectId = "1"; }
             <tr>
                 <td><?php echo htmlspecialchars($subject['subject_id']);?></td>
                 <td class="table-cell-question"><input class="input-form input-100" type="text" name="<?php echo $subject['subject_id'];?>" value="<?php echo htmlspecialchars($subject['description']);?>"/></td>
-                <td><input type="image" src="images/delete-icon.png" alt="Delete" height="45" width="45" class="alignBottomImg cursor" onclick="deleteSubject(event, <?php echo $question['question_id'];?>)"/></td>
+                <td><input type="image" src="images/delete-icon.png" alt="Delete" height="40" width="40" class="alignBottomImg cursor" onclick="deleteSubject(event, <?php echo $subject['subject_id'];?>)"/></td>
             </tr>
         <?php } ?>
         </tbody>
     </table>
 	<div class="center">
-        <button type="submit" class="btn-basic center" name="add" >Add Subject</button>
+        <button type="submit" class="btn-basic center" name="add" onclick="addSubject(event);">Add Subject</button>
     </div>
 
 <?php } ?>
@@ -109,7 +155,7 @@ if (!isset($subjectId)) { $subjectId = "1"; }
                 <td><?php echo htmlspecialchars($question['question_id']);?></td>
                 <td class="table-cell-question"><button class="collapsible" onclick="toggleOptions(this);return false;" value="<?php echo $question['question_id'];?>"><?php echo htmlspecialchars($question['description']);?></button></td>
                 <td><input type="image" src="images/arrow-down-icon.png" alt="Collapse" height="45" width="45" class="alignBottomImg cursor" value="<?php echo $question['question_id'];?>" onclick="toggleOptions(this);return false;" id="<?php echo $question['question_id'];?>"/></td>
-                <td><input type="image" src="images/delete-icon.png" alt="Delete" height="45" width="45" class="alignBottomImg cursor" onclick="deleteQuestion(event, <?php echo $question['question_id'];?>)"/></td>
+                <td><input type="image" src="images/delete-icon.png" alt="Delete" height="40" width="40" class="alignBottomImg cursor" onclick="deleteQuestion(event, <?php echo $question['question_id'];?>)"/></td>
                 <td><input type="image" src="images/edit-icon.png" alt="Edit" height="30" width="30" class="alignBottomImg cursor" onclick="editQuestion(event, <?php echo $question['question_id'];?>)"/></td>
             </tr>
             <tr >
