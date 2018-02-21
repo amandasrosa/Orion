d10d4039be1084370b768cc3efe70ce413f29aa6
Denis Gois <denisdwtg@gmail.com>
2018-02-21 11:03:21 -0500

    Fix confirmation message position

--- userConfirmation.php
+++ userConfirmation.php
@@ -3,13 +3,13 @@ session_start();
 $userFullName = filter_input(INPUT_GET, 'userFullName');
 
 include 'view/header.php'; ?>
-    <div class="parent-user-confirmation">
-        <section class="center-section user-confirmation">
-            <form action="index.php" method="post" class="center">
-                <h1>Hey <?php echo $userFullName?>, you're registered!</h1>
-                <h2>Thanks for signing up for an Orion account!</h2>
-                <input type="submit" value="Redirect to Login Page" class="btn-basic">
-            </form>
-        </section>
-    </div>
+
+    <section class="center-section user-confirmation">
+        <form action="index.php" method="post" class="center">
+            <h1>Hey <?php echo $userFullName?>, you're registered!</h1>
+            <h2>Thanks for signing up for an Orion account!</h2>
+            <input type="submit" value="Redirect to Login Page" class="btn-basic">
+        </form>
+    </section>
+
 <?php include 'view/footer.php'; ?>
